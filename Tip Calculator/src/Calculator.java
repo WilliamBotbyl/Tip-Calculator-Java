4a6ff1b2a9855e985dbba6050a62113de5e44311
billybotbyl <billybotbyl@localhost>
2022-02-01 21:00:23 -0500

    Final Version

--- Tip Calculator/src/Calculator.java
+++ Tip Calculator/src/Calculator.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
-
 		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the bill amount.");
+		double initialBill = input.nextDouble();
+
+		System.out.println("Enter the gratuity rate in a percent form, i.e.  10 or 20");
+		double tipRate = input.nextDouble();
+
+		input.close();
+
+		double tipAmount = tipRate / 100 * initialBill;
+
+		double totalBill = tipAmount + initialBill;
+		System.out.printf("The total amount due is %.2f", totalBill);
 
 	}
 
